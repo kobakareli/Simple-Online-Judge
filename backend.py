110bf8719853c74a97ee3d3dab33a3150cb53c9d
Tornike Mandzulashvili <tornikeman@gmail.com>
2016-06-14 04:58:14 +1200

    Skeleton implementation

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ def compile_code(file):
     if os.path.isfile(class_file):
         os.remove(class_file)
     if os.path.isfile(file):
-        os.system('gcc -o '+class_file+' '+file)
+        os.system('g++ -std=c++11 -o '+class_file+' '+file)
         if os.path.isfile(class_file):
             return 200
         else:
@@ -15,28 +15,34 @@ def compile_code(file):
     else:
         return 404
 
-
 def run_code(file, input_file, output_file, timeout, mem_limit):
-    cmd = './'+file
-    r = os.system('timeout '+timeout+' '+cmd+' < '+input_file+' > ' + output_file)
-    os.remove(file)
+    print(file)
+    print(input_file)
+    print(output_file)
+
+    class_file = file[:-4]
+    os.system('g++ -std=c++11 -o ' + class_file + ' ' + file)
+    cmd = os.getcwd() + "/" + class_file
+    r = os.system('timeout '+ timeout +' ' + cmd + ' < '+input_file+' > ' + output_file)
+    print(r)
+
     if r == 0:
         return 200
     elif r == 31744:
-        os.remove(output_file)
+        #os.remove(output_file)
         return 408
     else:
-        os.remove(output_file)
+        #os.remove(output_file)
         return 400
 
 
 def match(output_file, answer):
     if os.path.isfile(output_file) and os.path.isfile(answer):
-        b = filecmp.cmp(output_file, answer)
-        os.remove(output_file)
+        f1 = open(output_file, "r")
+        f2 = open(answer, "r")
+        b = f1.read() == f2.read()
+        #b = filecmp.cmp(output_file, answer)
+        #os.remove(output_file)
         return b
     else:
         return 404
-
-compile_code('bla.cpp')
-
--- website.py
+++ website.py
@@ -1,21 +1,92 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-app = Flask(__name__)
+import backend
+import os
+import subprocess
 
+app = Flask(__name__)
+INPUT_SUFFIX = ""
+OUTPUT_SUFFIX = "a"
+TIME_LIMIT = "2s"
+MEMORY_LIMIT = 64 #MB
+INPUT_FILE_NAME = "aligator.in"
+OUTPUT_FILE_NAME = "aligator.out"
 
 @app.route("/")
 def main():
     return render_template('index.html')
 
 
+def is_suffix(test, suffix):
+    if suffix == "":
+        if "." in test:
+            return False
+        else:
+            return True
+    return test.endswith(suffix)
+
+
+def check_test(test, filename):
+    open(OUTPUT_FILE_NAME, "w")
+
+    run_result = backend.run_code(filename, test[0], os.getcwd() + "/" + OUTPUT_FILE_NAME, TIME_LIMIT, MEMORY_LIMIT)
+    if run_result == 408:
+        return "TIME OUT"
+    if run_result == 400:
+        return "RUNTIME ERROR"
+
+    match_result = backend.match(os.getcwd() + "/" + OUTPUT_FILE_NAME, test[1])
+    if match_result == False:
+        return "WRONG ANSWER"
+
+    return "OK"
+
 @app.route('/result/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['datafile']
-        f.save(f.filename)
-        # TODO run tests here and return result
-        return 'file uploaded successfully'
+        f.save(os.getcwd() + "/" + f.filename)
+
+        compile_result = backend.compile_code(f.filename)
+        if (compile_result == 400):
+            return "COMPILATION_ERROR"
+
+        FULL_PATH = os.getcwd() + "/PycharmProjects/Simple-Online-Judge/tests/"
+
+        tests = os.listdir(FULL_PATH)
+        tests.sort()
+
+        test_pairs = list()
+        timer = 0
+
+        for test in tests:
+            if not (is_suffix(test, INPUT_SUFFIX) or is_suffix(test, OUTPUT_SUFFIX)):
+                continue
+            if timer % 2 == 0:
+                test_pairs.append(["", ""])
+            test_pairs[len(test_pairs) - 1][timer % 2] = test
+            timer += 1
+
+        for i in range(len(test_pairs)):
+            if is_suffix(test_pairs[i][0], OUTPUT_SUFFIX):
+                tmp = test_pairs[i][0]
+                test_pairs[i][0] = test_pairs[i][1]
+                test_pairs[i][1] = tmp
+
+            test_pairs[i]=[FULL_PATH + test_pairs[i][0] , FULL_PATH + test_pairs[i][1]]
+
+        print(test_pairs)
+
+        id = 0
+        for test in test_pairs:
+            id += 1
+            print(id)
+            test_result = check_test(test, f.filename)
+            if test_result != "OK":
+                return test_result
+
+        return "OK"
 
 if __name__ == "__main__":
     app.run()
