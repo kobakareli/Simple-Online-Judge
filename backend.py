c76cfc2bf7c506d9930bb5d4a4aefa549ed9efba
Koba <kkare13@freeuni.edu.ge>
2016-06-13 13:32:17 +0400

    add code compiling and running

---
+++ backend.py
@@ -0,0 +1,40 @@
+import os
+import filecmp
+
+
+def compile_code(file):
+    class_file = file[:-4]
+    if os.path.isfile(class_file):
+        os.remove(class_file)
+    if os.path.isfile(file):
+        os.system('g++ -o '+class_file+' '+file)
+        if os.path.isfile(class_file):
+            return 200
+        else:
+            return 400
+    else:
+        return 404
+
+
+def run_code(file, input_file, output_file, timeout, mem_limit):
+    cmd = './'+file
+    r = os.system('timeout '+timeout+' '+cmd+' < '+input_file+' > ' + output_file)
+    os.remove(file)
+    if r == 0:
+        return 200
+    elif r == 31744:
+        os.remove(output_file)
+        return 408
+    else:
+        os.remove(output_file)
+        return 400
+
+
+def match(output_file, answer):
+    if os.path.isfile(output_file) and os.path.isfile(answer):
+        b = filecmp.cmp(output_file, answer)
+        os.remove(output_file)
+        return b
+    else:
+        return 404
+
